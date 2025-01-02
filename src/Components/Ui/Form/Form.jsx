ce697b3346b2f91c6b4ebf1943bb2645ddc3c6a4
marimorita <marimm0927@gmail.com>
2025-01-02 11:36:11 -0500

    dropdown menu

--- src/Components/Ui/Form/Form.jsx
+++ src/Components/Ui/Form/Form.jsx
@@ -10,7 +10,16 @@
         <Inputs placeholder={"Dirección"} />
         <Inputs placeholder={"Teléfono"} />
         <Inputs placeholder={"Cargo"} />
-        <Inputs placeholder={"Rol"} />
+        <div className='flex items-center justify-center'>
+         <select className='focus:text-black bg-white p-2 rounded w-[65%] border border- h-18 font-semibold text-[#9ca3af]'>
+          <option value="Rol" disabled selected>Rol</option>
+          <option value="Técnica"className='text-black'>Técnica </option>
+          <option value="Administrativa"className='text-black'>Administrativa</option> 
+          <option value="Juridica"className='text-black'>Jurídica </option>
+          <option value="Consulta"className='text-black'>Consulta</option>
+          <option value="Administrador" className='text-black'>Administrador</option>
+        </select>
+        </div>
       </form>
     )
   }
