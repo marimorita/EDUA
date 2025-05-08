4d2960d1f1db1ea196ee57f614481f586bdd5d86
marimorita <marimm0927@gmail.com>
2025-05-08 09:05:26 -0500

    feat(procedures): Add new procedures

---
+++ procedures.sql
@@ -0,0 +1,10 @@
+-- Procedimiento para registarr una solicitud desde la recepcion --
+Delimiter $$
+create procedure solicitudRecepcion (
+	in radicado i
+    in archivoPdf text ,
+    in ccReceptionist int
+)
+Begin
+	select * from 
+End$$
